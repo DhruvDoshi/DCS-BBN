19d210f1971614b919904e078a8d43c81d0d9527
Dhruv Doshi <41964565+DhruvDoshi@users.noreply.github.com>
2019-07-16 16:47:50 +0530

    Create And Dump Keys
    This program gives us the public and the private keys which is further used in how the blockchain transaction works

---
+++ wallet.py
@@ -0,0 +1,99 @@
+'''
+Here in this file we are working with the wallets and the major part of the hashing and
+ssecurity consideration is done is here we are also working with SHA256 and PKCS1_v1_5 algorithm to 
+genrate the Signature of the transaction
+
+
+use case = "Major use"
+made: 22/5
+no_of updated = 127
+last_update: 18/6
+
+******THERE WILL ALWAYS BE ONE ERROR IN LAST LINE*******
+******CAN'T DO ANYTHING ABOUT THAT********
+
+
+'''
+
+
+from Crypto.PublicKey import RSA
+#this will generate the unique public key
+from Crypto.Signature import PKCS1_v1_5
+#this will generate the signature (special algorithm)
+from Crypto.Hash import SHA256
+#security
+import Crypto.Random
+import binascii
+
+
+class Wallet:
+    """Creates, loads and holds private and public keys. Manages transaction signing and verification."""
+
+    def __init__(self, node_id):
+        self.private_key = None
+        self.public_key = None
+        self.node_id = node_id
+
+    def create_keys(self):
+        """Create a new pair of private and public keys."""
+        private_key, public_key = self.generate_keys()
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def save_keys(self):
+        """Saves the keys to a file (wallet.txt)."""
+        if self.public_key != None and self.private_key != None:
+            try:
+                with open('wallet-{}.txt'.format(self.node_id), mode='w') as f:
+                    f.write(self.public_key)
+                    f.write('\n')
+                    f.write(self.private_key)
+                return True
+            except (IOError, IndexError):
+                print('Saving wallet failed...')
+                return False
+
+    def load_keys(self):
+        """Loads the keys from the wallet.txt file into memory."""
+        try:
+            with open('wallet-{}.txt'.format(self.node_id), mode='r') as f:
+                keys = f.readlines()
+                public_key = keys[0][:-1]
+                private_key = keys[1]
+                self.public_key = public_key
+                self.private_key = private_key
+            return True
+        except (IOError, IndexError):
+            print('Loading wallet failed...')
+            return False
+
+    def generate_keys(self):
+        """Generate a new pair of private and public key."""
+        private_key = RSA.generate(1024, Crypto.Random.new().read)
+        public_key = private_key.publickey()
+        return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'), binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
+
+    def sign_transaction(self, sender, recipient, amount):
+        """Sign a transaction and return the signature.
+
+        Arguments:
+            :sender: The sender of the transaction.
+            :recipient: The recipient of the transaction.
+            :amount: The amount of the transaction.
+        """
+        signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
+        h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
+        signature = signer.sign(h)
+        return binascii.hexlify(signature).decode('ascii')
+
+    @staticmethod
+    def verify_transaction(transaction):
+        """Verify the signature of a transaction.
+
+        Arguments:
+            :transaction: The transaction that should be verified.
+        """
+        public_key = RSA.importKey(binascii.unhexlify(transaction.sender))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA256.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
+        return verifier.verify(h, binascii.unhexlify(transaction.signature))
